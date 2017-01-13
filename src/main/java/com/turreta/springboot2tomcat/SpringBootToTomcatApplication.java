8db2adccf07085d9be825e8d3160beb3b61c7a13
Turreta.com <Turreta@users.noreply.github.com>
2017-01-13 10:47:20 +0800

    Add files via upload

---
+++ src/main/java/com/turreta/springboot2tomcat/SpringBootToTomcatApplication.java
@@ -0,0 +1,12 @@
+package com.turreta.springboot2tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootToTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootToTomcatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/turreta/springboot2tomcat/SpringBootToTomcatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.turreta.springboot2tomcat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootToTomcatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
